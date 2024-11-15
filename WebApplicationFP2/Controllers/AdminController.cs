d039e2fdb781831ab0286dcc8c3cd92217380e2c
dhrtiradha <diana.roze.lv@gmail.com>
2024-11-15 21:55:11 +0200

    done

--- WebApplicationFP2/Controllers/AdminController.cs
+++ WebApplicationFP2/Controllers/AdminController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using WebApplicationFP2.Models;
+using WebApplicationFP2.Storage;
 
 namespace WebApplicationFP2.Controllers
 {
@@ -14,6 +16,46 @@ namespace WebApplicationFP2.Controllers
         public IActionResult GetFlight(int id)
         {
             return NotFound();
+        }
+
+        [HttpPost]
+        [Route("flights")]
+        public IActionResult AddFlight(Flight flight)
+        {
+            if (flight.From == null || flight.To == null || string.IsNullOrEmpty(flight.Carrier) ||
+                string.IsNullOrEmpty(flight.DepartureTime) || string.IsNullOrEmpty(flight.ArrivalTime) ||
+                string.IsNullOrEmpty(flight.From.AirportCode) || string.IsNullOrEmpty(flight.To.AirportCode))
+            {
+                return BadRequest("Invalid flight data."); 
+            }
+
+            if (string.Equals(flight.From.AirportCode.Trim(), flight.To.AirportCode.Trim(), StringComparison.OrdinalIgnoreCase))
+            {
+                return BadRequest("Departure and destination airports cannot be the same.");
+            }
+
+            if (DateTime.TryParse(flight.DepartureTime, out var departureTime) &&
+                DateTime.TryParse(flight.ArrivalTime, out var arrivalTime))
+            {
+                if (arrivalTime <= departureTime)
+                {
+                    return BadRequest("Arrival time must be after departure time.");
+                }
+            }
+            else
+            {
+                return BadRequest("Invalid date format.");
+            }
+
+            if (FlightStorage.FlightExists(flight)) 
+            {
+                return Conflict("The flight already exists."); 
+            }
+            else
+            {
+                var addedFlight = FlightStorage.AddFlight(flight); 
+                return Created("", addedFlight);
+            }
         }
     }
 }
---
+++ WebApplicationFP2/Storage/FlightStorage.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+using WebApplicationFP2.Models;
+
+namespace WebApplicationFP2.Storage
+{
+    public static class FlightStorage
+    {
+        private static List<Flight> _flights = new List<Flight>();
+        private static int _id = 0;
+
+        public static Flight AddFlight(Flight flight)
+        {
+            flight.Id = ++_id;
+            _flights.Add(flight);
+
+            return flight;
+        }
+
+        public static void ClearFlights()
+        {
+            _flights.Clear();
+        }
+
+        public static bool FlightExists(Flight flight)
+        {
+            return _flights.Any(f =>
+                f.From.AirportCode == flight.From.AirportCode &&
+                f.To.AirportCode == flight.To.AirportCode &&
+                f.Carrier == flight.Carrier &&
+                f.DepartureTime == flight.DepartureTime &&
+                f.ArrivalTime == flight.ArrivalTime);
+        }
+    }
+}
